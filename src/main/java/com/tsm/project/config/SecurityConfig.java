28c1014c5fc9886f62761295a1dbce49db8a283c
Kitsana <Kitsana@Kitsana-PC>
2016-02-02 14:44:46 +0700

    login-db

--- src/main/java/com/tsm/project/config/SecurityConfig.java
+++ src/main/java/com/tsm/project/config/SecurityConfig.java
@@ -1,6 +1,11 @@
 package com.tsm.project.config;
 
-//@Configuration
-//@ImportResource(value = "classpath:spring/security-config.xml")
-public class SecurityConfig {}
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+	
+}
 
